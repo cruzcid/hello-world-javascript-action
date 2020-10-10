a0f9f0fed6282ea6112f2686d0bf3863e2bfb279
cruzcid <cruzcid@users.noreply.github.com>
2020-10-10 11:18:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 
-  const payload = JSON.stringtify(github.context.payload, undefined, 2);
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
