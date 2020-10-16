0a555d59057612f27b677de1f51b4d0930731f2a
Cruze Castro <cruz.castro@matchcraft.com>
2020-10-15 22:49:10 -0500

    Attempt to write pull request list in file on git

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
+const token = core.getInput('token');
 
 try {
   const nameToGreet = core.getInput('who-to-greet');
@@ -11,13 +12,30 @@ try {
   //const payload = JSON.stringify(github.context.payload, undefined, 2);
   //console.log(`The event payload: ${payload}`);
 
-  fs.writeFile('FirstFileCreated', 'Brande es big', 'utf-8', (err) => {
-    if (err) {
-      throw err;
-    }
+  let prom = pullRequests(rip.context.repo.owner, rip.context.repo.repo);
 
-    console.log('README update complete.');
+  prom.then(pulls =>{
+    fs.writeFile('FirstFileCreated', pulls, 'utf-8', (err) => {
+
+      if (err) {
+        throw err;
+      }
+      console.log('README update complete.');
+
+    });
   });
 } catch (error) {
   core.setFailed(error.message);
+}
+
+function pullRequests(repoOwner, repo){
+  let pr = github.Github(token);
+  let resp = pr.pulls.list({
+    owner: repoOwner,
+    repo: repo
+  })["catch"](function (e) {
+    console.log(e.message);
+  });
+
+  return resp;
 }
