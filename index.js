73a4624458fd9c7c67aded9302b4d334610a9299
Cruz Castro <cruz.castro@matchcraft.com>
2020-10-20 23:06:28 -0500

    Octokit usage

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
 const token = core.getInput('token');
+const { Octokit } = require("@octokit/rest");
+
 
 try {
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+
 
   //const payload = JSON.stringify(github.context.payload, undefined, 2);
   //console.log(`The event payload: ${payload}`);
@@ -29,7 +32,7 @@ try {
 }
 
 function pullRequests(repoOwner, repo){
-  const octokit = new github.GitHub(token);
+  const octokit = new Octokit();
   let resp = octokit.pulls.list({
     owner: repoOwner,
     repo: repo
