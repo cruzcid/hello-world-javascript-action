b51ec93db0223d88737ad2814e0d5fa9367a0500
cruzcid <abstractime@outlook.com>
2020-11-18 15:10:58 -0600

    Head-branch-name: output index

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 
-  const githubBranchName = core.getInput('github-head-branch-name');
-  console.log(`github-head-branch-name: ${githubBranchName}`);
-
   let prom = pullRequests(github.context.repo.owner, github.context.repo.repo);
 
   prom.then(pullsList => {
@@ -30,6 +27,8 @@ try {
       }
     }
 
+    console.log("head-branch-name: ->", contributorsList[0].head_ref);
+    core.setOutput("head-branch-name", contributorsList[0].head_ref);
     const skillContributorList = JSON.stringify(contributorsList, undefined, 2);
 
     fs.readFile('README.md', 'utf-8', (err, data) => {
